79380d06622299cd3194c49adcc1c3e01c0a8a2a
benlesko <blesko521@gmail.com>
2023-05-23 09:21:16 -0700

    Implemented local storage functionality.

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 // Input text field listener
-
 document.addEventListener('DOMContentLoaded', function() {
-    var taskForm = document.getElementById('taskForm');
+    let taskForm = document.getElementById('taskForm');
+    let taskInput = document.getElementById('taskInput');
+    let taskList = document.getElementById('taskList');
+
+    // Loading tasks from local storage - initialize an empty array if nothing is there
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    // Display tasks in the list
+    tasks.forEach(function(taskText) {
+        createTaskElement(taskText);
+    });
+
+    // Event handler for when the user adds a new task
     taskForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-      addTask();
+        event.preventDefault();
+        addTask();
     });
   
     function addTask() {
-      var input = document.getElementById('taskInput');
-      var taskList = document.getElementById('taskList');
+        // Triming and storing our input text
+        let taskText = taskInput.value.trim();
 
-      // checking if the user actually inputted a task (no empty input)
-      if (input.value.trim() !== '') {
-        // creating a new task and add it to the list
-        var task = document.createElement('li');
-        task.appendChild(document.createTextNode(input.value));
-        taskList.appendChild(task);
-        // reset input text
-        input.value = '';
-      }
+        // checking if the user actually inputted a task (no empty input)
+        if (taskText !== '') {
+            createTaskElement(taskText);
+
+            // Save task to local storage
+            tasks.push(taskText);
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            
+            // Reset input text
+            taskInput.value = '';
+        }
     }
+    
+    // Creates a new text element and adds the task to our list
+    function createTaskElement(taskText) {
+        var task = document.createElement('li');
+        task.appendChild(document.createTextNode(taskText));
+        taskList.appendChild(task);
+      }
   });
+
+//Clear storage
+//localStorage.clear();
